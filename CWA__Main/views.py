46afc06c22399c7ad5413486645746b426c6087a
j-lh-t <7lhtuan@gmail.com>
2022-04-24 08:57:15 +0700

    Comment something

--- CWA__Main/views.py
+++ CWA__Main/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#Todo: Đây là Trang chủ 
 def index(request):
     return render(request,"main/index.html")
--- CWA__Sign/views.py
+++ CWA__Sign/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#Todo: Đây là hàm trang Đăng nhập
 def Sign_In(request):
     return render(request, "sign/Sign_In.html")
